b0181460c1b2bbde9c14609b62b61e04cf34cbd2
Isaac Lee <isaacleekyexen@gmail.com>
2025-08-26 17:49:52 +0800

    Echo
    Echo commands entered by the user and exit when the user types the command "bye"

--- src/main/java/JohnChatter.java
+++ src/main/java/JohnChatter.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class JohnChatter {
     public static void main(String[] args) {
-        System.out.println("hello, i am john chatter");
-        System.out.println("goodbye!");
+        System.out.println("it is i, john chatter");
+
+        String input = "";
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("goodbye!");
+                return;
+            } else {
+                System.out.println(input);
+            }
+        }
     }
 }
