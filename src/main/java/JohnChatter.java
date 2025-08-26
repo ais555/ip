f289915bf7fe488f57b3974ea05e89d64c58650c
Isaac Lee <isaacleekyexen@gmail.com>
2025-08-26 18:02:56 +0800

    Add the "Add" and "List" features
    Add ability to store text entered by user into a list and display them back when requested with the "list" command

--- src/main/java/JohnChatter.java
+++ src/main/java/JohnChatter.java
@@ -4,6 +4,9 @@ public class JohnChatter {
     public static void main(String[] args) {
         System.out.println("it is i, john chatter");
 
+        String[] items = new String[100];
+        int nextItem = 0;
+
         String input = "";
         Scanner scanner = new Scanner(System.in);
         while (true) {
@@ -11,8 +14,16 @@ public class JohnChatter {
             if (input.equals("bye")) {
                 System.out.println("goodbye!");
                 return;
+            } else if (input.equals("list")) {
+                for (int i = 1; i <= items.length; i++) {
+                    if (items[i -1] != null) {
+                        System.out.println(i + ". " + items[i - 1]);
+                    }
+                }
             } else {
-                System.out.println(input);
+                System.out.println("added: " + input);
+                items[nextItem] = input;
+                nextItem++;
             }
         }
     }
