87b2c2c7ce4fbeb3a94cc31045a42241cbbdf1a6
Isaac Lee <isaacleekyexen@gmail.com>
2025-08-26 23:01:59 +0800

    Add "Mark as Done"
    Add a "Task" class and the ability to mark a task as done or undone with the "mark" and "unmark" commands.

--- src/main/java/JohnChatter.java
+++ src/main/java/JohnChatter.java
@@ -4,25 +4,47 @@ public class JohnChatter {
     public static void main(String[] args) {
         System.out.println("it is i, john chatter");
 
-        String[] items = new String[100];
+        Task[] items = new Task[100];
         int nextItem = 0;
 
         String input = "";
         Scanner scanner = new Scanner(System.in);
         while (true) {
+            // Wait for user inputs. Breaks out and returns when the command "bye" is given
             input = scanner.nextLine();
+            String[] splitInput = input.split(" ");
+
             if (input.equals("bye")) {
                 System.out.println("goodbye!");
                 return;
             } else if (input.equals("list")) {
                 for (int i = 1; i <= items.length; i++) {
-                    if (items[i -1] != null) {
-                        System.out.println(i + ". " + items[i - 1]);
+                    Task item = items[i - 1];
+                    if (item != null) {
+                        System.out.println(i + ".[" + item.getStatusIcon() + "] " + item.description);
                     }
+                }
+            } else if (splitInput.length == 2 && splitInput[0].equals("mark") && splitInput[1].matches("\\d+")) {
+                // Mark a task as done
+                int index = Integer.parseInt(splitInput[1]);
+                if (items[index - 1] != null) {
+                    items[index - 1].markAsDone();
+                    System.out.println("marked " + items[index - 1].description + " as done");
+                } else {
+                    System.out.println("i cannot mark a task that doesn't exist yet");
+                }
+            } else if (splitInput.length == 2 && splitInput[0].equals("unmark") && splitInput[1].matches("\\d+")) {
+                // Mark a task as undone
+                int index = Integer.parseInt(splitInput[1]);
+                if (items[index - 1] != null) {
+                    items[index - 1].markAsUndone();
+                    System.out.println("marked " + items[index - 1].description + " as undone");
+                } else {
+                    System.out.println("i cannot mark a task that doesn't exist yet");
                 }
             } else {
                 System.out.println("added: " + input);
-                items[nextItem] = input;
+                items[nextItem] = new Task(input);
                 nextItem++;
             }
         }
