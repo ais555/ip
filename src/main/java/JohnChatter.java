c08f19a880e6bbab96ff7ba0fd7f8a73ccba2428
Isaac Lee <isaacleekyexen@gmail.com>
2025-08-28 01:18:15 +0800

    Implement Todos, Deadlines and Events

--- src/main/java/JohnChatter.java
+++ src/main/java/JohnChatter.java
@@ -12,7 +12,7 @@ public class JohnChatter {
         while (true) {
             // Wait for user inputs. Breaks out and returns when the command "bye" is given
             input = scanner.nextLine();
-            String[] splitInput = input.split(" ");
+            String[] splitInputAroundSpace = input.split(" ");
 
             if (input.equals("bye")) {
                 System.out.println("goodbye!");
@@ -24,28 +24,42 @@ public class JohnChatter {
                         System.out.println(i + "." + item.toString());
                     }
                 }
-            } else if (splitInput.length == 2 && splitInput[0].equals("mark") && splitInput[1].matches("\\d+")) {
+            } else if (splitInputAroundSpace.length == 2 && splitInputAroundSpace[0].equals("mark") && splitInputAroundSpace[1].matches("\\d+")) {
                 // Mark a task as done
-                int index = Integer.parseInt(splitInput[1]);
+                int index = Integer.parseInt(splitInputAroundSpace[1]);
                 if (items[index - 1] != null) {
                     items[index - 1].markAsDone();
                     System.out.println("marked " + items[index - 1].description + " as done");
                 } else {
                     System.out.println("i cannot mark a task that doesn't exist yet");
                 }
-            } else if (splitInput.length == 2 && splitInput[0].equals("unmark") && splitInput[1].matches("\\d+")) {
+            } else if (splitInputAroundSpace.length == 2 && splitInputAroundSpace[0].equals("unmark") && splitInputAroundSpace[1].matches("\\d+")) {
                 // Mark a task as undone
-                int index = Integer.parseInt(splitInput[1]);
+                int index = Integer.parseInt(splitInputAroundSpace[1]);
                 if (items[index - 1] != null) {
                     items[index - 1].markAsUndone();
                     System.out.println("marked " + items[index - 1].description + " as undone");
                 } else {
                     System.out.println("i cannot mark a task that doesn't exist yet");
                 }
-            } else {
-                System.out.println("added: " + input);
-                items[nextItem] = new Task(input);
+            } else if (splitInputAroundSpace[0].equals("todo")) {
+                Todo todo = new Todo(input.split("todo ")[1]);
+                items[nextItem] = todo;
                 nextItem++;
+                System.out.println("added:\n" + todo.toString());
+            } else if (splitInputAroundSpace[0].equals("deadline")) {
+                Deadline deadline = new Deadline(input.split("deadline ")[1].split(" /by")[0], input.split("/by ")[1]);
+                items[nextItem] = deadline;
+                nextItem++;
+                System.out.println("added:\n" + deadline.toString());
+            } else if (splitInputAroundSpace[0].equals("event")) {
+                String end = input.split("/to ")[1];
+                String start = input.split(" /to")[0].split("/from ")[1];
+                String description = input.split(" /to")[0].split(" /from")[0].split("event ")[1];
+                Event event = new Event(description, start, end);
+                items[nextItem] = event;
+                nextItem++;
+                System.out.println("added:\n" + event.toString());
             }
         }
     }
