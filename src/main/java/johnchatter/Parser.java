0b34754b20f9b42358d81e2c15290cddbcc629c6
Isaac Lee <isaacleekyexen@gmail.com>
2025-09-19 02:01:55 +0800

    Avoid magic Strings

--- src/main/java/johnchatter/Parser.java
+++ src/main/java/johnchatter/Parser.java
@@ -10,6 +10,17 @@ import java.util.stream.Collectors;
  * Handles the parsing of commands from user input.
  */
 public class Parser {
+    private static final String ERROR_TODO_NO_DESCRIPTION =
+            "oops! todo must have a description";
+    private static final String ERROR_DEADLINE_NO_BY =
+            "oops! deadline must have a /by date";
+    private static final String ERROR_EVENT_NO_FROM_TO =
+            "oops! event must have /from and /to dates";
+    private static final String ERROR_INVALID_TASK_NUMBER =
+            "it seems you have input an invalid task number, please check and try again";
+    private static final String ERROR_UNKNOWN_COMMAND =
+            "sorry, i don't know what that means";
+
     public static String parse(String input, Ui ui, Storage storage, TaskList tasks) throws JohnChatterException {
         assert input != null : "Input should not be null";
         assert ui != null : "Ui should not be null";
@@ -40,7 +51,7 @@ public class Parser {
         case "delete":
             return handleDelete(tokens, list, tasks, storage, ui);
         default:
-            throw new JohnChatterException("sorry, i don't know what that means");
+            throw new JohnChatterException(ERROR_UNKNOWN_COMMAND);
         }
     }
 
@@ -88,7 +99,7 @@ public class Parser {
 
     private static String handleTodo(String input, Storage storage, TaskList tasks, Ui ui) throws JohnChatterException {
         if (!input.contains("todo ")) {
-            throw new JohnChatterException("oops! tasks must have a description");
+            throw new JohnChatterException(ERROR_TODO_NO_DESCRIPTION);
         }
         Todo todo = new Todo(input.split("todo ")[1]);
 
@@ -97,7 +108,7 @@ public class Parser {
 
     private static String handleDeadline(String input, Storage storage, TaskList tasks, Ui ui) throws JohnChatterException {
         if (!input.contains("/by ")) {
-            throw new JohnChatterException("oops! deadline must have a /by");
+            throw new JohnChatterException(ERROR_DEADLINE_NO_BY);
         }
         String description = input.split("deadline ")[1].split(" /by")[0];
         String deadlineDateInput = input.split("/by ")[1];
@@ -109,7 +120,7 @@ public class Parser {
 
     private static String handleEvent(String input, Storage storage, TaskList tasks, Ui ui) throws JohnChatterException {
         if (!input.contains("/from ") || !input.contains("/to ")) {
-            throw new JohnChatterException("oops! event must have /from and /to");
+            throw new JohnChatterException(ERROR_EVENT_NO_FROM_TO);
         }
         String description = input.split(" /from")[0].split("event ")[1];
         String startInput = input.split(" /to")[0].split("/from ")[1];
@@ -130,11 +141,11 @@ public class Parser {
 
     private static int parseIndex(String[] tokens, ArrayList<Task> list) throws JohnChatterException {
         if (tokens.length != 2 || !tokens[1].matches("\\d+")) {
-            throw new JohnChatterException("please provide a valid task number");
+            throw new JohnChatterException(ERROR_INVALID_TASK_NUMBER);
         }
         int index = Integer.parseInt(tokens[1]) - 1;
         if (index < 0 || index >= list.size()) {
-            throw new JohnChatterException("it seems you have input an invalid task number, please check and try again");
+            throw new JohnChatterException(ERROR_INVALID_TASK_NUMBER);
         }
         return index;
     }
